a81fb0f6d916ac4bd6bebe6ea0d8c8a596c08055
yexilein <stephischer@gmail.com>
2018-06-22 10:24:00 -0400

    Add parsing module for ATAC data

---
+++ ecker.R
@@ -0,0 +1,16 @@
+library(Matrix)
+
+BICCN_DIR <- "/home/fischer/data/biccn/raw_data_180521"
+ATAC_DIR <- file.path(BICCN_DIR, "EckerCallaway_snATAC-Seq/v2_20180525")
+
+ecker_atac_gene_counts <- function() {
+  result <- read.table(file.path(ATAC_DIR, "gene_count_table", "MOp_raw_gene_count_table.txt.gz"), header=TRUE)
+  rownames(result) <- result$barcode
+  result <- subset(result, select = -barcode)
+  return(t(Matrix(as.matrix(result), sparse = TRUE)))
+}
+
+ecker_atac_metadata <- function(level=4) {
+  metadata <- read.table(file.path(ATAC_DIR, "MOp_multi_level_cluster.csv"), sep = ",", header = TRUE)
+  return(data.frame(cell_id = metadata$barcode, cluster_label = metadata[, level+1]))
+}
